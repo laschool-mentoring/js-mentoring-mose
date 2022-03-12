9567db31a5419997544435c86efbcc476d50f3a7
younggyo <lee4519698@hanmail.net>
2022-03-12 17:54:17 +0900

    3월 1주차 멘토링 실습

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
+// 1. 출력 함수 -> console.log
 console.log("Initial commit");
+
+// 2. 상수, 변수 선언 -> 값을 바꿔보는
+const name = "이영교";
+console.log(name);
+
+// 상수는 다시 선언할 수 없음
+// name = "오모세" -> error
+
+let age = 23;
+console.log(age);
+
+age = 22;
+console.log(age);
+
+console.log(age + 1);
+console.log("22" + 1);
