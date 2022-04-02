2fc103004804a1d4835a935b1f6f8e71fe8789e9
younggyo <lee4519698@hanmail.net>
2022-04-02 21:20:04 +0900

    3월 4주차 멘토링

---
+++ Code/3월 4주차.js
@@ -0,0 +1,17 @@
+// 3월 4주차 멘토링 출력
+
+// 배열 사과 배 귤 귤-> 출력
+
+// for 구구단 9단 break 3단
+
+// while 
+
+// function 구구단 
+
+// async await
+
+// pip
+
+// npm node package management  ->  import numpy pandas keras tenserflow 
+
+// npm -> nodejs -> npm init -> ›
